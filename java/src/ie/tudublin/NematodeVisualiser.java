e9a623b3c3ea655f9e5c15d4c1b27802fec9be57
C20441826 Conor Davis <c20441826@mytudublin.ie>
2022-04-27 17:00:49 +0100

    Created loadNematodes method to load from csv

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -8,6 +8,9 @@ import processing.data.TableRow;
 public class NematodeVisualiser extends PApplet
 {
 
+	//declare array list of nematode objects
+	ArrayList<Nematode> nematodes = new ArrayList<Nematode>();
+
 	public void keyPressed()
 	{		
 		if (keyCode == LEFT)
@@ -19,6 +22,7 @@ public class NematodeVisualiser extends PApplet
 	public void settings()
 	{
 		size(800, 800);
+		loadNematodes();//load csv file and populate ArrayList
 	}
 
 	public void setup() 
@@ -28,16 +32,24 @@ public class NematodeVisualiser extends PApplet
 		smooth();				
 	}
 	
-
+	//loadNematodes method
 	public void loadNematodes()
 	{
-
-	}
-
+		//load the csv file as table
+		Table nematodeTable = loadTable("../data/nematodes.csv","header");
+		//for loop to iterate through the table rows
+		for(TableRow row:nematodeTable.rows())
+		{
+			//create new nematode object passing the table row as a parameter
+			Nematode nematode = new Nematode(row);
+			//add it to array list
+			nematodes.add(nematode);
+		}//end for loop
+	}//end loadNematodes method
 
 	public void draw()
 	{
 
 	}
 
-}
+}//end class
