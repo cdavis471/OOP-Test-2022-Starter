7620c46d347c6e55e29088594f5225299040248d
C20441826 Conor Davis <c20441826@mytudublin.ie>
2022-04-27 18:09:27 +0100

    Added limbs, gender, eyes

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -56,6 +56,12 @@ public class NematodeVisualiser extends PApplet
 		colorMode(HSB);
 		background(0);
 
+		//nematode setup
+		int length = nematodes.get(selectedNematode).getNemLength();
+		int limbs = nematodes.get(selectedNematode).getNemLimbs();
+		String gender = nematodes.get(selectedNematode).getNemGender();
+		int eyes = nematodes.get(selectedNematode).getNemEyes();
+
 		//name
 		textSize(20);
 		textAlign(CENTER);
@@ -67,10 +73,36 @@ public class NematodeVisualiser extends PApplet
 		fill(0,0,0);
 		strokeWeight(3);
 		stroke(255,255,200);
-		int length = nematodes.get(selectedNematode).getNemLength();
 		for(int i = 1;i <= length;i++)
 		{
+
+			//limbs
+			if(limbs == 1)
+			{
+				line(width/2 + 50, (height/8) + (50*i), width/2 - 50, (height/8) + (50*i));
+			}//end if statement
+			//gender part 1
+			if(((gender.contains("m") == true) || (gender.contains("h") == true))&& i == length)
+			{
+				line(width/2,(height/8) + (50*i),width/2,(height/8) + (50*i) + 40);
+				ellipse(width/2,(height/8) + (50*i) + 40,10,10);
+			}//end if statement
+			//eyes
+			if(eyes == 1 && i == 1)
+			{
+				line(width/2,(height/8) + (50*i),width/2+30,(height/8) + (50*i) - 40);
+				line(width/2,(height/8) + (50*i),width/2-30,(height/8) + (50*i) - 40);
+				ellipse(width/2+30,(height/8) + (50*i) - 40,10,10);
+				ellipse(width/2-30,(height/8) + (50*i) - 40,10,10);
+			}//end if statement
+			//main body
 			ellipse(width/2,(height/8) + (50*i),50,50);
+			//gender part 2
+			if(((gender.contains("f") == true) || (gender.contains("h") == true))&& i == length)
+			{
+				ellipse(width/2,(height/8) + (50*i),25,25);
+			}//end if statement
+
 		}//end for loop
 
 	}//end draw method
